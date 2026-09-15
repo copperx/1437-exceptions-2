a832abfaf45702d2a0bea6ffb1b10f23335b9b03
Rafael Escalante <copperx@users.noreply.github.com>
2026-09-15 08:05:30 -0600

    Update StepPractice.java

--- StepPractice.java
+++ StepPractice.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class StepPractice {
     public static double stepsToMiles(int steps) {
         // TODO: Throw IllegalArgumentException if steps is negative.
-        if(steps < 0) {
-            throw new IllegalArgumentException("Steps is negative");
-        }
+        
 
         return steps / 2000.0;
     }
@@ -18,16 +16,12 @@ public class StepPractice {
 
         // TODO: Handle invalid numeric text and negative step counts.
         // Handle NumberFormatException, then IllegalArgumentException
-        try {
-            int steps = Integer.parseInt(text);
-            double miles = stepsToMiles(steps);
+        
+        int steps = Integer.parseInt(text);
+        double miles = stepsToMiles(steps);
 
-            System.out.println("Miles: " + miles);
-        } catch(NumberFormatException e) {
-            System.out.println("You must enter a number");
-        } catch(IllegalArgumentException e) {
-            System.out.println("Steps must be positive");
-        }
+        System.out.println("Miles: " + miles);
+        
         
     }
 }
